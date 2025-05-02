85f8aa04b4100b21551bbb8515ecf0c40f02a05a
Riku Inoue <153576638+Ruri1212@users.noreply.github.com>
2025-05-03 01:06:23 +0900

    Implement annotation feature as per Issue #2

---
+++ src/annotation.js
@@ -0,0 +1,10 @@
+// 新しいアノテーション機能を実装するコード
+
+function addAnnotation(adId, annotation) {
+    // ...existing code...
+    console.log(`Adding annotation to ad ${adId}: ${annotation}`);
+    // ...existing code...
+}
+
+// 例: アノテーションを追加する
+addAnnotation(1, 'This is a sample annotation.');
